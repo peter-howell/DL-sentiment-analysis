15e8ef768fd80519e6c2192ff6d21e55ba67aa51
peter-howell <44275653+peter-howell@users.noreply.github.com>
2023-05-03 13:04:28 -0400

    Update encode_text.py

--- encode_text.py
+++ encode_text.py
@@ -1,6 +1,6 @@
 """
-Code that uses text vectorization to encode the reviews as sequences of word indices
-and stores the vocabulary found from the training data
+Code for Task 2 that uses text vectorization to encode the reviews as sequences of word indices
+and stores the vocabulary found from the training data 
 """
 
 import numpy as np
@@ -38,6 +38,7 @@ sequence_len = 250 # max length of review
 # adapt the encoder with the training data
 encoder = TextVectorization(max_tokens=max_tokens, output_sequence_length=sequence_len)
 encoder.adapt(reviews)
+
 print('encoder adapted')
 
 # saving the encoder's vocabulary so it can be used for adapting later if needed
