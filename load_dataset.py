6155b98fb0c061274c819eb5060a71c0dfa16ff9
peter-howell <44275653+peter-howell@users.noreply.github.com>
2023-05-03 13:04:44 -0400

    Update load_dataset.py

--- load_dataset.py
+++ load_dataset.py
@@ -1,5 +1,6 @@
 """
-Code that loads the dataset and stores the relevant features in temporary files
+Code used in task 1 and 2 to load and process datasets from files
+For task 2: loads the dataset and stores the relevant features in temporary files
 Extracts and cleans the text of reviews as well as the review ratings
 Splits the data 80/20 into training/testing
 """
@@ -7,13 +8,12 @@ import json
 import re
 from string import punctuation
 
-import nltk
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.model_selection import train_test_split
-nltk.download('stopwords')
-nltk.download('punkt')
+# nltk.download('stopwords') - only need to run once on a machine
+# nltk.download('punkt') - only need to run once on a machine
 
 stop_words = set(stopwords.words('english'))    
 
@@ -43,42 +43,117 @@ def clean_text(text):
 
     return text
 
-# Loading the review data from json file 
-data = []
-with open('yelp_dataset/yelp_academic_dataset_review.json', 'r') as f:
-    for line in f:
-        data.append(json.loads(line))
+def load_review_dataset(fname='/Users/peterhowell/Downloads/yelp_dataset/yelp_academic_dataset_review.json') -> pd.DataFrame:
+    """Load the review json file at `fname` into a dataframe
+    
+    Args:
+        fname (str): name of review json file
+    Returns:
+        pd.DataFrame: dataframe of reviews
+    """
+    # Loading the review data from json file 
+    rev_data = []
+    with open(fname, 'r') as f:
+        for line in f:
+            rev_data.append(json.loads(line))
 
-df = pd.DataFrame(data)
-del data
-print('have dataframe')
+    review_df = pd.DataFrame(rev_data)
+    del rev_data
+    print('review dataframe loaded')
+    return review_df
 
-df.drop(['review_id','user_id',	'business_id','useful','funny','cool', 'date'], axis=1, inplace=True)
+# Task 1
+def load_business_dataset(fname='business.json') -> dict:
+    """loads data stored in file `fname` and returns list of businesses and their attributes
 
-df['stars'] = df['stars'].astype(int)
+    Args:
+        fname (str, optional): file name. Defaults to 'business.json'.
 
-# ## Identifying and extracting target
+    Returns:
+        dict: dictionary with business_id as keys and attributes as values
+    """
+    bus_data = {}
+    with open(fname, 'r') as f:
+        for line in f:
+            json_line = json.loads(line.strip())
+            attrs = json_line['attributes']
+            
+            # only include businesses that have at least one attribute
+            if  attrs is not None:
+                bus_attrs = {}
+                # check if each attribute is a boolean
+                for attr, value in attrs.items():
+                    # check for string storage of boolean
+                    if isinstance(value, str) and value.lower() in ["true", "false"]:
+                        value = value.lower() == "true"
+                    if isinstance(value, bool):
+                         # store boolean as int, 0 or 1
+                        bus_attrs[attr] = int(value)
+                    if bus_attrs:
+                        bus_data[json_line['business_id']] = bus_attrs
+    print('business dataset loaded')
+    return bus_data
 
-# currently using:
-# convert star rating to    negative, neutral,  positive
-#                           [1,0,0]   [0,1,0]   [0,0,1]
+def clean_reviews(review_df: pd.DataFrame) -> pd.DataFrame:
+    """applies `clean_text` to the reviews the dataframe
 
-df['negative'] = (df['stars'] < 3).astype(int)
-df['neutral'] = (df['stars'] == 3).astype(int)
-df['positive'] = (df['stars'] > 3).astype(int)
+    Args:
+        reviews (pd.DataFrame): reviews to clean
 
-df.drop(['stars'], axis=1, inplace=True)
+    Returns:
+        pd.DataFrame: dataframe with the reviews cleaned
+    """
+    print('beginning to clean reviews')
+    review_df['text'] = review_df['text'].apply(clean_text)
+    print('reviews cleaned')
+    return review_df
 
-df['text'] = df['text'].apply(clean_text)
+def process_reviews(review_df: pd.DataFrame) -> pd.DataFrame:
+    """Processes reviews for use in task 2 by removing columns, cleaning the text, etc
 
-train, test = train_test_split(df, train_size=0.8, random_state=42)
-print('train shape: ', train.shape)
-print('test shape:', test.shape)
+    Args:
+        review_df (pd.DataFrame): Review dataframe
 
-train[['negative', 'neutral', 'positive']].to_pickle('data/train_labels')
-train[['text']].to_pickle('data/train_text')
-print('train data saved')
+    Returns:
+        pd.DataFrame: processed review dataframe
+    """
+    review_df.drop(['review_id','user_id',	'business_id','useful','funny','cool', 'date'], axis=1, inplace=True)
 
-test[['negative', 'neutral', 'positive']].to_pickle('data/test_labels')
-test[['text']].to_pickle('data/test_text')
-print('test data saved')
+    review_df['stars'] = review_df['stars'].astype(int)
+    # ## Identifying and extracting target
+
+    # currently using:
+    # convert star rating to:   negative, neutral,  positive
+    #                           [1,0,0]   [0,1,0]   [0,0,1]
+    review_df['negative'] = (review_df['stars'] <  3).astype(int)
+    review_df['neutral']  = (review_df['stars'] == 3).astype(int)
+    review_df['positive'] = (review_df['stars'] >  3).astype(int)
+
+    review_df.drop(['stars'], axis=1, inplace=True)
+
+    review_df = clean_reviews(review_df)
+    return review_df
+
+def dump_reviews(df: pd.DataFrame):
+    """splits dumps dataset into files for later usage
+
+    Args:
+        df (pd.DataFrame): dataframe
+    """
+    train, test = train_test_split(df, train_size=0.8, random_state=42)
+    print('train shape: ', train.shape)
+    print('test shape:', test.shape)
+
+    train[['negative', 'neutral', 'positive']].to_pickle('data/train_labels')
+    train[['text']].to_pickle('data/train_text')
+    print('train data saved')
+
+    test[['negative', 'neutral', 'positive']].to_pickle('data/test_labels')
+    test[['text']].to_pickle('data/test_text')
+    print('test data saved')
+
+if __name__ == '__main__':
+    # Task 2
+    review_df = load_review_dataset()
+    review_df = process_reviews(review_df)
+    dump_reviews(review_df)
